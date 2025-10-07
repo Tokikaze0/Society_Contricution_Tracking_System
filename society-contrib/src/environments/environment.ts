f0e48ced8964ef53d14bea9ac47c80e57192a53b
Arwin <rwinmadeja@gmail.com>
2025-10-07 10:14:42 +0800

    chore: update package.json dependencies and devDependencies; replace Firebase config in environment.ts

--- society-contrib/src/environments/environment.ts
+++ society-contrib/src/environments/environment.ts
@@ -4,15 +4,16 @@
 
 export const environment = {
   production: false,
-  // TODO: Replace with your Firebase project config
-  firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID',
-  },
+  firebaseConfig: {
+    apiKey: "AIzaSyAqKWttEvwVm9tQ3dyUp201K30wl3JIDxw",
+    authDomain: "ionic-iot-lab-c5c2b.firebaseapp.com",
+    databaseURL: "https://ionic-iot-lab-c5c2b-default-rtdb.firebaseio.com",
+    projectId: "ionic-iot-lab-c5c2b",
+    storageBucket: "ionic-iot-lab-c5c2b.firebasestorage.app",
+    messagingSenderId: "640561089083",
+    appId: "1:640561089083:web:80d615bb3134c274406a1e",
+    measurementId: "G-97QEJZT5HN"
+  }
 };
 
 /*
